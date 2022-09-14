1c0fdd5b5644cc87f6ff9ce602a43698cb561a3c
Bakhtiyorov Sarvarbek <bakhtiyorov.sarvar2001@gmail.com>
2022-09-14 20:03:52 +0500

    CONFIG:  Add Startup.cs and StorageBroker Transient Service

--- Sheenam.Core.Api/Program.cs
+++ Sheenam.Core.Api/Program.cs
@@ -3,28 +3,45 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Hosting;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Sheenam.Core.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>());
+        }
+    }
 }
+//var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+//// Add services to the container.
 
-app.UseAuthorization();
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
-app.MapControllers();
+//var app = builder.Build();
 
-app.Run();
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
---
+++ Sheenam.Core.Api/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.OpenApi.Models;
+using Sheenam.Core.Api.Brokers.Storages;
+
+namespace Sheenam.Core.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<StorageBroker>();
+            AddBrokers(services);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(
+                    name: "v1",
+                    info: new OpenApiInfo
+                    {
+                        Title = "Sheenam.Core.Api",
+                        Version = "v1"
+                    }
+                );
+            });
+        }
+
+        public void Configure(
+            IApplicationBuilder applicationBuilder,
+            IWebHostEnvironment webHostEnvironment)
+        {
+            if (webHostEnvironment.IsDevelopment())
+            {
+                applicationBuilder.UseDeveloperExceptionPage();
+                applicationBuilder.UseSwagger();
+                applicationBuilder.UseSwaggerUI(options =>
+
+                options.SwaggerEndpoint(
+                    url: "/swagger/v1/swagger.json",
+                    name: "Sheenam.Core.Api v1"));
+            }
+
+            applicationBuilder.UseHttpsRedirection();
+            applicationBuilder.UseRouting();
+            applicationBuilder.UseAuthorization();
+
+            applicationBuilder.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+        }
+
+        private static void AddBrokers(IServiceCollection services)
+        {
+            services.AddTransient<IStorageBroker, StorageBroker>();
+        }
+    }
+}
